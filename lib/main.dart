2cb55f6a2bb63068100a964592cbdbba99d1592d
Aman Verma <amanvermadirect@gmail.com>
2022-02-21 15:37:33 +0530

    Personality test app done

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/result.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    // 'What\'s your favourite color?',
+    // 'What\'s your favourite animal?',
+    {
+      'questionText': 'What\'s your favourite color?',
+      'answer': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favourite animal?',
+      'answer': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snale', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9},
+      ],
+    },
+    {
+      'questionText': 'Who\'s your favourite instructor?',
+      'answer': [
+        {'text': 'Shaktiman', 'score': 1},
+        {'text': 'Tamraj Kilwish', 'score': 10},
+        {'text': 'Batman', 'score': 5},
+        {'text': 'SUperman', 'score': 8},
+      ],
+    },
+  ];
+
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore = _totalScore + score;
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print('We have more questions');
+    } else {
+      print('No more questions');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("My First App"),
-        ),
-        body: Text("This is my default text"),
-      ),
+          appBar: AppBar(
+            title: Text("Personality test app"),
+          ),
+          body: _questionIndex < _questions.length
+              ? Quiz(
+                  answerQuestion: _answerQuestion,
+                  questionIndex: _questionIndex,
+                  questions: _questions,
+                )
+              : Result(_totalScore, _resetQuiz)),
     );
   }
 }
